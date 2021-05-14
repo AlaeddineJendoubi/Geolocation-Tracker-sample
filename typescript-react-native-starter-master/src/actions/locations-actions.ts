b48050a2d787f51c77c1e9a28d06e7435e33a9b8
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 11:51:18 +0100

    Update locations-actions.ts

--- typescript-react-native-starter-master/src/actions/locations-actions.ts
+++ typescript-react-native-starter-master/src/actions/locations-actions.ts
@@ -1,4 +1,4 @@
-import {createAsyncAction} from 'typesafe-actions';
+import {createAsyncAction, createAction} from 'typesafe-actions';
 import {Location, Coordinates} from 'src/types';
 
 export const fetchLocationAsync = createAsyncAction(
@@ -6,4 +6,12 @@ export const fetchLocationAsync = createAsyncAction(
   ['LOCATION_FETCH_SUCCESS', (res: Location) => res],
   ['LOCATION_ERROR', (err: Error) => err],
   'LOCATION_FETCH_CANCEL',
+)();
+export const deleteAllLocationsAsync = createAction('LOCATION_DELETE_ALL', () => [])();
+export const delLocation = createAction(
+  'LOCATION_DEL',
+  (locationId: string, location?: Location) => ({
+    locationId,
+    location,
+  }),
 )();
