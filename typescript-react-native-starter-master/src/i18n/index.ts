eee75eab3476dff40d3dc49d380079fe0310f0e0
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 11:10:00 +0100

    add internalisations

--- typescript-react-native-starter-master/src/i18n/index.ts
+++ typescript-react-native-starter-master/src/i18n/index.ts
@@ -7,26 +7,20 @@ const resources = {
     translation: {
       english: 'English',
       french: 'Français',
-      welcome: 'Welcome to Typescript React Native Starter!',
-      instructions: 'To get started, edit Home.tsx',
-      iosInstruction: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-      androidInstruction:
-        'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-      settings: 'Settings',
-      home: 'Home',
-      fetchUser: 'Fetch user',
+      title: 'Location manager',
+      previousLocation: 'Previous locations',
+      recentLocation: 'Recent location',
+      delete: 'DELETE',
+      deleteAll: 'DELETE ALL LOCATIONS',
     },
   },
   fr: {
     translation: {
-      welcome: 'Bienvenue à Typescript React Native Starter !',
-      instructions: 'Pour commencer, éditez Home.tsx',
-      iosInstruction: 'Appuyez sur Cmd+R pour recharger,\n' + 'Cmd+D ou secouez pour le menu dev',
-      androidInstruction:
-        'Appuyez deux fois sur la touche R de votre clavier pour recharger,\n' +
-        'Secouer ou appuyer sur le bouton de menu pour le menu de développement',
-      settings: 'Paramètres',
-      home: 'Accueil',
+      title: "Gestionnaire de l'emplacement",
+      previousLocation: 'Emplacements précédents',
+      recentLocation: 'Emplacement récent',
+      delete: 'EFFACER',
+      deleteAll: 'SUPPRIMER TOUS LES EMPLACEMENTS',
     },
   },
 };
