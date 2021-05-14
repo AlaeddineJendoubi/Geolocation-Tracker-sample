dc9943008930c3e4acb8915bc4c0ac0312bffefe
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 11:46:13 +0100

    prepare native location module

---
+++ typescript-react-native-starter-master/ios/AaquaLocationManager.h
@@ -0,0 +1,18 @@
+//
+//  AaquaLocationManager.h
+//  TypescriptReactNativeStarter
+//
+//  Created by alaeddine jendoubi on 14/05/2021.
+//
+
+#ifndef AaquaLocationManager_h
+#define AaquaLocationManager_h
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+
+@interface AaqualocationManager : RCTEventEmitter
+
+@end
+
+#endif /* AaquaLocationManager_h */
---
+++ typescript-react-native-starter-master/ios/AaquaLocationManager.m
@@ -0,0 +1,8 @@
+//
+//  AaquaLocationManager.m
+//  TypescriptReactNativeStarter
+//
+//  Created by alaeddine jendoubi on 14/05/2021.
+//
+
+#import <Foundation/Foundation.h>
