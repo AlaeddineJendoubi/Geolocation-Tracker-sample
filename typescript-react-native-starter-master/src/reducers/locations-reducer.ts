36caf012d5b410c6d9e41f161f44a00add845ea8
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 10:56:47 +0100

    create locations reducers

---
+++ typescript-react-native-starter-master/src/reducers/locations-reducer.ts
@@ -0,0 +1,31 @@
+'use strict';
+import {fetchLocationAsync} from 'src/actions/location-actions';
+import {Location} from 'src/types';
+import {createReducer} from 'typesafe-actions';
+import {LocationAction} from 'src/actions/actionTypes';
+import cuid from 'cuid';
+
+export interface LocationState {
+  locations?: Location[];
+}
+
+const initialState: LocationState = {
+  locations: [],
+};
+
+const locationReducer = createReducer<LocationState, LocationAction>(initialState).handleAction(
+  fetchLocationAsync.success,
+  (state, action) => ({
+    ...state,
+    locations: [
+      {
+        ...action?.payload,
+        id: cuid(),
+        date: String(new Date()),
+      },
+      ...state?.locations,
+    ],
+  }),
+);
+
+export default locationReducer;
