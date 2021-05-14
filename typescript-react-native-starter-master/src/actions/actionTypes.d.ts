1a8293ca625410a1ce3c15bc9dc0e6ff5a0648ca
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 10:56:00 +0100

    add locations action type

--- typescript-react-native-starter-master/src/actions/actionTypes.d.ts
+++ typescript-react-native-starter-master/src/actions/actionTypes.d.ts
@@ -1,8 +1,8 @@
 import {ActionType} from 'typesafe-actions';
 import * as appActions from './appActions';
 import * as usersActions from './usersActions';
-
+import * as locationsActions from './locations-actions';
 export type AppAction = ActionType<typeof appActions>;
 export type UsersAction = ActionType<typeof usersActions>;
-
+export type LocationAction = ActionType<typeof locationsActions>;
 export type RootAction = AppAction | UsersAction;
