2a815fdc169c6a314257e080e08d34cc81142e49
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2021-05-14 12:39:03 +0100

    Update delete-location-button.tsx

--- typescript-react-native-starter-master/src/components/locations-list/delete-location-button.tsx
+++ typescript-react-native-starter-master/src/components/locations-list/delete-location-button.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Button} from '@ui-kitten/components';
 import {useTranslation} from 'react-i18next';
+import {useDispatch} from 'react-redux';
+import {deleteLocationAction} from "../../actions/locations-actions"
+
+
+
 
 export const DeleteLocationButton = (locationID:string) => {
+  
+  // ************ Hooks initialisation ************************
   const {t} = useTranslation();
+  const dispatch = useDispatch();
+
+  // ************ Action dispatch  ************************
+  /**
+   * Deletes a locations by specifing it's id from the state
+   * @returns {LocationState} a new location state array without the provided location
+   */
   const deleteLocationFromState = () => {
-    console.log('deleted');
+    dispatch(deleteLocationAction(locationID));
   };
   return <Button size="tiny" onPress={() => deleteLocationFromState()}>  {t('delete')}  </Button>;
 };
+
+
